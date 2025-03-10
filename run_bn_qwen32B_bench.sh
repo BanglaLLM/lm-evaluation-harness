8f5a424fa085d58921799f69c799f5e4c2346710
Andy Khan (Ananda) <brishtiteveja@gmail.com>
2025-03-10 18:03:52 -0500

    extra bench for 32B qwen

---
+++ run_bn_qwen32B_bench.sh
@@ -0,0 +1,25 @@
+limit=1.0
+
+models="\
+BanglaLLM/Bangla-s1k-qwen-2.5-32B-Instruct,\
+Qwen/Qwen2.5-32B-Instruct"
+
+# limit=0.1
+# models="BanglaLLM/Bangla-s1k-qwen-2.5-3B-Instruct"
+
+python scripts/bangla_lm_benchmark.py \
+--models ${models} \
+--batch_size "auto:4" \
+--num_fewshot 0 \
+--device "cuda:0" \
+--acc_norm true \
+--output_path "outputs"
+
+
+python scripts/bangla_lm_benchmark.py \
+--models ${models} \
+--batch_size "auto:4" \
+--num_fewshot 5 \
+--device "cuda:0" \
+--acc_norm true \
+--output_path "outputs"
