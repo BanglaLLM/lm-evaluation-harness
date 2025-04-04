e20032a265f2e2cd014d23a65917f97603a411ce
Andy Khan (Ananda) <brishtiteveja@gmail.com>
2025-04-04 01:42:37 -0500

    Added benchmark script. Added bangla math eval task and updated hotpotqa bn task eval

--- run_bn_qwq32B_bench.sh
+++ run_bn_qwq32B_bench.sh
@@ -1,14 +1,4 @@
 limit=1.0
-models="\
-BanglaLLM/Bangla-s1k-qwen-2.5-3B-Instruct,\
-Qwen/Qwen2.5-3B-Instruct,\
-BanglaLLM/Bangla-s1k-llama-3.2-3B-Instruct,\
-meta-llama/Llama-3.2-3B-Instruct,\
-hishab/titulm-llama-3.2-3b-v2.0"
-
-models="\
-BanglaLLM/Bangla-s1k-qwen-2.5-32B-Instruct,\
-Qwen/Qwen2.5-32B-Instruct"
 
 models="\
 BanglaLLM/Bangla-s1k-QWQ-32B-Instruct,\
